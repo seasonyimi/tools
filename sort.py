2dd1fea487231114b7966c3510ab5494dd61e4ab
seasonyimi <840806506@qq.com>
2021-05-27 23:34:29 +0800

    简单排序和堆排序

--- sort.py
+++ sort.py
@@ -61,3 +61,62 @@ def shellSort(nums):
     return nums
 list =  [5,12,9,4,6,7,1]
 print(shellSort(list))
+
+#简单选择排序
+def selsectd_sort(array):
+    #获取list的长度
+    length = len(array)
+    #进行比较的论数
+    for i in range(0,length-1):
+        smallest = i #默认设置最小值的index为当前值
+        #用当前最小的index的值分别与后面的值进行比较，以便获取最小index
+        for j in range(i+1,length):
+            #如果找到当前值分别与后面的值进行比较，则进行两值交换
+            if array[j] < array[smallest]:
+                array[j],array[smallest] = array[smallest],array[j]
+    return array
+lst = [3,12,5,59,35,5,4,31,64,7,6,25]
+print(selsectd_sort(lst))
+
+#堆排序
+def sift_down(array,start,end):
+    """"
+    调整成大顶堆，初始堆时，从下往下，交换堆顶与堆尾后，从上往下调整
+    :param array: 列表的引用
+    :param start:父结点
+    :param end:结束的下标
+    :return:无
+    """
+    while True:
+        #当列表第一个是以下标0开始，结点下标为i，左孩子则为2*i+1,右孩子下标则为2*i+2
+        #若下标为i开始，左孩子为2*i,右孩子则为2*i+1
+        left_child = 2 * start + 1  #左孩子的结点下标
+        #当结点的右孩子存在，则大于结点的左孩子时
+        if left_child > end:
+            break
+        if left_child+1 <= end and array[left_child+1] > array[left_child]:
+            left_child += 1
+        if array[left_child] > array[start]:
+            #当左右孩子的最大值大于父节点时，则交换
+            array[left_child],array[start] = array[start],array[left_child]
+            start = left_child #交换之后以交换子节点为跟的堆可能不是大顶堆，需重新调整
+        else:   #若父节点大于左右孩子，则退出循环
+            break
+def heap_sort(array):
+    #先初始化大顶堆
+    first = len(array)//2 - 1 #最后一个有孩子的节点（//表示取整)
+    #第一个结点下标为0
+    for i in range(first,-1,-1):
+        print(array[i])
+        sift_down(array,i,len(array)-1)  #初始化大顶堆
+        print("初始化堆顶结果：",array)
+        #交换堆顶与堆尾
+        for heap_end in range(len(array)-1,0,-1):
+            array[heap_end],array[0] = array[0],array[heap_end]
+            sift_down(array,0,heap_end-1)
+if __name__ == '__main__':
+    array = [25,24,5,36,89,1,5,25]
+    print(array)
+    heap_sort(array)
+    print("堆排序最终结果",array)
+
