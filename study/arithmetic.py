88873f8c498a7c3dd730aed1b4ad76c806221f28
seasonyimi <840806506@qq.com>
2022-06-15 20:20:04 +0800

    1.新增了列表的相关使用方法 2.新增了字符串的相关使用方法 3.新增集的相关方法 4.新增字典的相关方法 5.新增格式化修改符的相关方法 6.牛顿迭代法 7.fraction类（1.转换输出类型;2.四则运算;3.运算结果简化） 8.继承

--- study/arithmetic.py
+++ study/arithmetic.py
@@ -109,4 +109,60 @@ f2 = Fraction(1,2)
 f3 = f1+ f2
 print("f3=",f3)
 
-#继承
+'''继承'''
+#父类
+class LogicGate:
+    def __int__(self,n):
+        self.label = n
+        self.output = None
+    def getLabel(self):
+        return  self.label
+    def getOutput(self):
+        self.output = self.performFateLogic()
+        return self.output
+#取得两个输入值
+class BinaryGate(LogicGate):
+    def __init__(self,n):
+        super().__init__(n)
+        self.pinA = None
+        self.pinB = None
+    def getPinA(self):
+        return int(input("Enter Pin A input for gate" + self.getLabel() + "-->"))
+    def getPinB(self):
+        return  int(input("Enter Pin B input for gate" + self.getLabel() + "--->"))
+#一个输入值
+class UnaryGate(LogicGate):
+    def __init__(self,n):
+        super().__init__()   #不用实例化就可以调用父类
+        self.pin = None
+    def getPin(self):
+        return int(input("Enter Pin input for gate" + self.getLabel() + "--->"))
+#添加布尔运算
+class AndGate(BinaryGate):
+    def __init__(self,n):
+        super().__init__(n)
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 1 and b == 1:
+            return 1
+        else:
+            return 0
+class OrGate(BinaryGate):
+    def __init__(self,n):
+        super().__init__(n)
+    def performGateLogic(self):
+        a = self.getPinA()
+        b = self.getPinB()
+        if a == 1 or b == 1:
+            return 1
+        else:
+            return 0
+class Connector:
+    def __init__(self,fgate,tgate):
+        self.fromgate = fgate
+        self.togate = tgate
+    def getFrom(self):
+        return self.fromgate
+    def getTo(self):
+        return self.togate
